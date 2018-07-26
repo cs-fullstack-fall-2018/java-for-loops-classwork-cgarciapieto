075f9412d1f2a6e9ba4feb5c924581bf81f91291
Carlos Garcia <cgarciapieto@gmail.com>
2018-07-26 09:38:11 -0500

    java-for-loops-classwork-cgarciapieto.iml

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int lastNumber = 100;
+        System.out.println("Starting Count to " + lastNumber);
+        countTheNumbers(lastNumber);
+    }
+
+    public static void countTheNumbers(int myKounter) {
+        for(int num = 0; num< 100;num++);
+           System.out.println(num);
+
+    }
+    }
+
+
+
---
+++ src/excercise2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class excercise2 {
+    public static void main(String[] args) {
+
+        String[] names = {"Bob","Kenn","Kevin","Erin"};
+        ArrayList<String> studentList = new ArrayList<String>();
+        studentList.addAll(Arrays.asList(names));
+        for (String students : studentList){
+            System.out.println(students);
+        }
+
+    }
+}
